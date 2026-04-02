9c9485b9b45b47b3d127dfae44ba10b87a68fa9b
ihexon <14349453+ihexon@users.noreply.github.com>
2026-04-02 15:25:58 +0800

    chore(libkrun): update libkrun & libkrunfw to mainline
    Signed-off-by: ihexon <14349453+ihexon@users.noreply.github.com>

--- scripts/build.go
+++ scripts/build.go
@@ -12,7 +12,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const assetsBase = "https://github.com/ihexon/revm-assets/releases/download/v2.0.9"
+const assetsBase = "https://github.com/ihexon/revm-assets/releases/download/v2.0.10"
 
 // run executes a command, inheriting stdout/stderr. If env is non-nil,
 // those vars are appended to the current environment.
