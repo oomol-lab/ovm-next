6fc9f9686b95dac72a807c587643fbfee544fd19
ihexon <14349453+ihexon@users.noreply.github.com>
2026-04-03 13:39:14 +0800

    refactor(libarchive): static linking of libarchive
    Signed-off-by: ihexon <14349453+ihexon@users.noreply.github.com>

--- pkg/libarchive/cgo_darwin_arm64.go
+++ pkg/libarchive/cgo_darwin_arm64.go
@@ -3,6 +3,14 @@
 package libarchive_go
 
 /*
-#cgo pkg-config: libarchive
+#cgo CFLAGS: -I/opt/homebrew/opt/libarchive/include
+#cgo LDFLAGS: /opt/homebrew/opt/libarchive/lib/libarchive.a
+#cgo LDFLAGS: /opt/homebrew/opt/xz/lib/liblzma.a
+#cgo LDFLAGS: /opt/homebrew/opt/zstd/lib/libzstd.a
+#cgo LDFLAGS: /opt/homebrew/opt/lz4/lib/liblz4.a
+#cgo LDFLAGS: /opt/homebrew/opt/libb2/lib/libb2.a
+#cgo LDFLAGS: /opt/homebrew/opt/bzip2/lib/libbz2.a
+#cgo LDFLAGS: /opt/homebrew/opt/zlib/lib/libz.a
+#cgo LDFLAGS: -liconv -lexpat
 */
 import "C"
